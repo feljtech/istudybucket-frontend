833d2b0371f504bcd708c8661c47481eda9c3784
Ndi Lionel <59184392+spykelion@users.noreply.github.com>
2021-09-15 18:03:54 +0100

    restructure page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import './App.css';
+import React from 'react';
+import { LockClosedIcon, EyeIcon, EyeOffIcon } from '@heroicons/react/solid'
+
 
 function App() {
   return (
-    <div className="App">
-       <h1>iStudyBucket </h1>
-    </div>
+    <div className="min-h-screen flex items-center justify-center bg-gray-100 py-12 px-4 sm:px-6 lg:px-8">
+     <h2 className="mt-6 text-center text-2xl font-bold text-blue-500">iStudyBucket.Go through the readme.md.</h2>
+  </div>
   );
 }
 
