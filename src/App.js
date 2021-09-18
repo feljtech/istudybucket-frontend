19750bced80306a43916891f6aa9c38441bc2ca7
Ndi Lionel <59184392+spykelion@users.noreply.github.com>
2021-09-18 20:26:56 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { LockClosedIcon, EyeIcon, EyeOffIcon } from '@heroicons/react/solid'
 function App() {
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100 py-12 px-4 sm:px-6 lg:px-8">
-     <h2 className="mt-6 text-center text-2xl font-bold text-blue-500">iStudyBucket.Go through the readme.md.</h2>
+     <h2 className="mt-6 text-center text-2xl font-bold text-green-500">iStudyBucket.Go through the readme.md.</h2>
   </div>
   );
 }
