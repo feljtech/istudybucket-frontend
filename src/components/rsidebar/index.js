ab385ed48245d7213cd7de37ba5652c024fd5c35
Ndi Lionel <59184392+spykelion@users.noreply.github.com>
2021-09-24 17:40:38 +0100

    [src\components\rsidebar\index.js]: created

---
+++ src/components/rsidebar/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const RSideBar = ({...props}) => {
+    return (
+        <div {...props}>
+            <p className="my-3">right</p>
+            <p className="my-3">right</p>
+            <p className="my-3">right</p>
+            <p className="my-3">right</p>
+            <p className="my-3">right</p>
+            <p className="my-3">right</p>
+        </div>
+    )
+}
+
+export default RSideBar;
