8b2ab973ceb416e1f2c8a66f8c45bf9ccab9d71b
Ndi Lionel <59184392+spykelion@users.noreply.github.com>
2021-09-27 22:12:06 +0100

    [src\components\lsidebar\index.js]: updated icons, added responsives

--- src/components/lsidebar/index.js
+++ src/components/lsidebar/index.js
@@ -1,14 +1,26 @@
 import React from 'react'
+import {
+    ChatIcon,
+    HeartIcon,
+    ShareIcon,
+    ClockIcon,DocumentIcon, BanIcon
+  } from "@heroicons/react/outline";
 
 const LSideBar = ({...props}) => {
     return (
         <div className="border fixed left-0 flex flex-col px-5 h-full w-1/4" {...props}>
-            <p className="my-3">left</p>
-            <p className="my-3">left</p>
-            <p className="my-3">left</p>
-            <p className="my-3">left</p>
-            <p className="my-3">left</p>
-            <p className="my-3">left</p>
+        <div className="flex justify-center">
+            <DocumentIcon className="text-green-400 w-12" /> 
+                <p className="self-center">Pate</p>
+            </div>
+            <div className="flex justify-center">
+            <DocumentIcon className="text-green-400 w-12" /> 
+                <p className="self-center">Pate</p>
+            </div>
+            <div className="flex justify-center">
+            <BanIcon className="text-green-400 w-12" /> 
+                <p className="self-center">Pate</p>
+            </div>
         </div>
     )
 }
