b6dd78258e69cd5225843806fbb242fdb62380ca
Ndi Lionel <59184392+spykelion@users.noreply.github.com>
2021-10-01 20:49:41 +0100

    [src/__suib__/Button.jsx]: change font-weight <Medium>

--- src/__sub__/Button.jsx
+++ src/__sub__/Button.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Button = ({ name, type,...props}) => {
     return (
-        <button type={type} className="rounded bg-green-500 hover:bg-green-600 text-white px-3 py-2 font-semibold mt-2" {...props}>
+        <button type={type} className="rounded bg-green-500 hover:bg-green-600 text-white px-3 py-2 font-medium mt-2" {...props}>
             {name}
         </button>
     )
