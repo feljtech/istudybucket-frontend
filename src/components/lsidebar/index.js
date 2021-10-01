0a4d9e74060cf2f8ae03a53381d3c3e162a5164f
Ndi Lionel <59184392+spykelion@users.noreply.github.com>
2021-10-01 20:44:05 +0100

    [/src/components/lsidebar/index.js] update icons

--- src/components/lsidebar/index.js
+++ src/components/lsidebar/index.js
@@ -1,25 +1,27 @@
-import React from 'react'
-import {
-    DocumentIcon, BanIcon,
-  } from "@heroicons/react/outline";
+import React from "react";
+import { DocumentIcon, BanIcon } from "@heroicons/react/outline";
+import Card from "../../__sub__/Card";
 
-const LSideBar = ({...props}) => {
-    return (
-        <div className="border fixed left-0 flex flex-col px-5 h-full w-1/4" {...props}>
-        <div className="flex justify-center">
-            <DocumentIcon className="text-green-400 w-12" /> 
-                <p className="self-center">Pate</p>
-            </div>
-            <div className="flex justify-center">
-            <DocumentIcon className="text-green-400 w-12" /> 
-                <p className="self-center">Pate</p>
-            </div>
-            <div className="flex justify-center">
-            <BanIcon className="text-green-400 w-12" /> 
-                <p className="self-center">Pate</p>
-            </div>
-        </div>
-    )
-}
+const LSideBar = ({ ...props }) => {
+  return (
+    <div
+      className="border fixed left-0 flex flex-col px-5 h-full w-1/4 overflow-y-auto"
+      {...props}
+    >
+      <div className="flex justify-center">
+        <DocumentIcon className="text-green-400 w-8" />
+        {/* <p className="self-center">Pate</p> */}
+      </div>
+      <div className="flex justify-center">
+        <DocumentIcon className="text-green-400 w-8" />
+        {/* <p className="self-center">Pate</p> */}
+      </div>
+      <div className="flex justify-center">
+        <BanIcon className="text-green-400 w-8" />
+        {/* <p className="self-center">Pate</p> */}
+      </div>
+    </div>
+  );
+};
 
 export default LSideBar;
