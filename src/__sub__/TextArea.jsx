a4072532700d3252c85d75ae230beb81efab8870
Ndi Lionel <59184392+spykelion@users.noreply.github.com>
2021-10-01 20:48:32 +0100

    [src/__suib__/TextArea.jsx]: add propse

--- src/__sub__/TextArea.jsx
+++ src/__sub__/TextArea.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 
+// change to resizable when text reaches a specific length
 const TextArea = ({...props}) => {
-    return (
-        <textarea {...props} className="outline-none resize-none block w-full rounded mt-3" name="textfield" id="" cols="30" rows="2">
+    return ( 
+        <textarea {...props} className="outline-none resize-none block rounded mt-3" name="textfield" id="" cols="40" rows="2">
 
         </textarea>
     )
