d2cf8245078992b21852b4959dc9a7bc5822cf17
Ndi Lionel <59184392+spykelion@users.noreply.github.com>
2021-10-06 12:28:16 +0100

    [src/page/bucket/Profile.jsx]: added modal view

--- src/pages/bucket/Profile.jsx
+++ src/pages/bucket/Profile.jsx
@@ -1,21 +1,34 @@
-import React from 'react'
-import Button from '../../__sub__/Button'
-import Card from '../../__sub__/Card'
+import React from "react";
+import ModalBase from "../../modals";
+import Button from "../../__sub__/Button";
+import Card from "../../__sub__/Card";
 
-const Profile = ({name, img, description, ...props}) => {
-    return (
-            <Card className="border text-center mt-3 w-2/3 rounded-md pb-4" {...props}>
-                <div className="w-full bottom-5 h-10 bg-green-400 rounded-t-md"></div>
-                <div className="flex my-3 flex-row justify-center">
-                    <img className="rounded-full h-40 w-40" src={img} alt={img} />
-                   
-                </div>
-                <h1 className="font-bold self-center ml-3">{name}</h1>
-                <Button name="Request to join" type="button" /> 
-                <p className="text-gray-400">{description}</p>
-               
-            </Card>        
-    )
-}
+const Profile = ({ name, img, description, ...props }) => {
+    const [showModal, setShowModal] = React.useState(true)
+  return showModal && (
+      <div className={showModal && "fixed top-5 left-0 w-full h-screen flex justify-center items-center backdrop-filter backdrop-blur backdrop-brightness-50 z-40" || ""}
+      >
+          <div
+				id="overlay"
+				onClick={()=>setShowModal(false)}
+				className="absolute h-screen w-full -z-10"
+			/>
+          <Card
+          className="border text-center mt-3 w-1/2 rounded-md pb-4 bg-white z-50"
+          {...props}
+        >
+          <div className="w-full h-10 bg-green-400 rounded-t-md"></div>
+          <div className="flex my-3 flex-row justify-center">
+            <img className="rounded-full h-40 w-40" src={img} alt={img.slice(14)} />
+          </div>
+          <h1 className="font-bold self-center ml-3">{name}</h1>
+          <Button name="Request to join" type="button" />
+          <p className="text-gray-500 select-all">{description}</p>
+        </Card>
+  
+      </div>
 
-export default Profile
+  );
+};
+
+export default Profile;
