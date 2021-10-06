2713b094fb65af250d061501ac246f7744d38d63
Ndi Lionel <59184392+spykelion@users.noreply.github.com>
2021-10-06 12:26:53 +0100

    [src/_sub_/TextArea.jsx]: modified rows and cols

--- src/__sub__/TextArea.jsx
+++ src/__sub__/TextArea.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 // change to resizable when text reaches a specific length
 const TextArea = ({...props}) => {
     return ( 
-        <textarea {...props} className="outline-none resize-none block rounded mt-3" name="textfield" id="" cols="40" rows="2">
+        <textarea {...props} className="outline-none resize-none block rounded mt-3" name="textfield" id="" cols="50" rows="3">
 
         </textarea>
     )
