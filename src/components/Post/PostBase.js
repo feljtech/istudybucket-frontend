54bc5a27ba66f799e422865fba2ec541af14a679
Ndi Lionel <59184392+spykelion@users.noreply.github.com>
2021-10-06 12:26:14 +0100

    [src/Post/PostBase.js]: added upvote and downvote icon"

--- src/components/Post/PostBase.js
+++ src/components/Post/PostBase.js
@@ -1,10 +1,11 @@
 import React from "react";
 import {
-  ChatIcon,
-  HeartIcon,
-  ShareIcon,
-  ClockIcon,
+  ChatIcon,  UserCircleIcon,
 } from "@heroicons/react/outline";
+import {
+  ArrowDownIcon,
+  ArrowUpIcon
+} from "@heroicons/react/solid";
 import me from "../../assets/img/me.jpeg";
 import TextArea from "../../__sub__/TextArea";
 import Button from "../../__sub__/Button";
@@ -16,35 +17,36 @@ const PostBase = ({ children, ...props }) => {
   return (
     <div className="rounded border px-5 py-2 mt-5" {...props}>
       <div className="flex justify-between mb-4">
-        <div className="flex">
-          <img src={me} alt="me" className="rounded-full w-12 h-12" />
-          <div className="block mx-2">
-            <p className="mr-3 font-bold">Spyke Lionel</p>
-            <p className="mr-3 text-gray-400 italic">@lionel</p>
+        <div className="flex ">
+        <UserCircleIcon className="text-green-400 w-8" />
+          <div className="block mx-2 self-center">
+            <p className="hidden mr-3 font-bold">Spyke Lionel</p>
+            <p className="mr-3 text-gray-500 self-center">@lionel</p>
           </div>
         </div>
-        <p className="text-gray-400 align-left italic self-center">lecturer</p>
+        <p className="text-gray-400 ">September 2, 2021</p>
+        <p className="hidden text-gray-400 align-left italic self-center">lecturer</p>
       </div>
       <div className="">{children}</div>
 
-      <div className="flex justify-between border-t">
-        <div className="flex">
-          <HeartIcon className="text-green-600 w-8 fill-current" />
-          <span className="text-green-600 self-center">24</span>
-          <span className="sr-only">likes</span>
+      <div className="flex justify-between border-t pt-2">
+        <div className="flex justify-between">
+          <div className="flex border border-green-300 rounded-full mr-2 p-1 cursor-pointer hover:bg-green-500 ">
+            <span className="text-green-600 hover:text-white text-sm self-center">24</span>
+            <ArrowUpIcon className="text-green-600 hover:text-white w-6 " />
+            <span className="sr-only">upvotes</span>
+          </div>
+          <div className="flex border border-green-300 rounded-full mr-2 p-1 cursor-pointer hover:bg-green-500 ">
+            <span className="text-green-600 hover:text-white text-sm self-center">24</span>
+            <ArrowDownIcon className="text-green-600 hover:text-white w-6 " />
+            <span className="sr-only">downvotes</span>
+          </div>
         </div>
         <div className="flex">
           <ChatIcon className="w-8 text-green-600" aria-hidden="true" />
-          <span className="text-green-500 self-center">24</span>
+          <span className="text-green-500 self-center">6</span>
           <span className="sr-only">comments</span>
         </div>
-        <div className="flex">
-          <ShareIcon className="w-8 text-green-600" />
-          <span className="sr-only">share</span>
-        </div>
-
-        {/* <ClockIcon className="text-green-400 w-8" /> */}
-        <p className="text-gray-400 ">September 2, 2021</p>
       </div>
 
       <form className="">
