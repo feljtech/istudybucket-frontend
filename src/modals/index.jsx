da8ce34bf0f4e8ae4def0056a466b6c62b8b4778
Ndi Lionel <59184392+spykelion@users.noreply.github.com>
2021-10-06 12:24:21 +0100

    [src/components/modal/ModalBase]: create modal base"

---
+++ src/modals/index.jsx
@@ -0,0 +1,32 @@
+import React, { useContext, cloneElement } from 'react'
+import { ModalContext } from '../components/api/context/ModalContext'
+
+const ModalBase = ({ children, title }) => {
+	const { modalData, setModalData } = useContext(ModalContext)
+	const closeModal = () => setModalData({ ...modalData, modalShow: false })
+	// debugger
+
+	return (
+		<div
+			id="modal-container"
+			className="fixed top-5 left-0 w-full h-screen flex justify-center items-center backdrop-filter backdrop-blur backdrop-brightness-50 z-40"
+		>
+			<div
+				id="overlay"
+				onClick={closeModal}
+				className="absolute h-screen w-full -z-10"
+			/>
+			<div className="bg-white w-6/7 md:w-3/5">
+				<div className="font-medium leading-6 py-6 px-7 flex items-center justify-between">
+					<h3>{title}</h3>
+				</div>
+				<div id="modal-child" className="p-2">
+					{/* Pass close function down */}
+					{cloneElement(children, { closeModal })}
+				</div>
+			</div>
+		</div>
+	)
+}
+
+export default ModalBase
