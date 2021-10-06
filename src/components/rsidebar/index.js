7bd840b4a10c592bc4893c2422c84a62dd3e0c1d
Ndi Lionel <59184392+spykelion@users.noreply.github.com>
2021-10-06 12:30:11 +0100

    [src/components/rsidebar/index.js]: change view[fontz, alignment]l

--- src/components/rsidebar/index.js
+++ src/components/rsidebar/index.js
@@ -5,72 +5,66 @@ const RSideBar = ({ ...props }) => {
   return (
     <div className="overflow-y-auto" {...props}>
       <div className="my-3">
+      <p className="font-semibold text-xl">Your buckets</p>
+      <Card>
+        <div className="flex justify-items-start my-3">
+          <img src={bulb} alt="bulb" className="w-14 h-14 rounded-full self-center" />
+          <div className="flex flex-col text-left self-center mx-2">
+          <p className="font-semibold text-md text-gray-600">The Light bulb</p>
+            <span className="text-xs">
+              A purposeful area for the formation....
+            </span>
+          </div>
+        </div> 
+      </Card>
         <Card>
-        <p className="font-semibold text-xl">Your buckets</p>
         <div className="flex justify-items-start my-3">
-          <img src={bulb} alt="bulb" className="w-14 h-14 rounded self-center" />
-          <div className="flex flex-col text-left mx-2">
-          <p className="font-semibold text-lg text-gray-600">The Light bulb</p>
-            <span className="hidden">
+          <img src={bulb} alt="bulb" className="w-14 h-14 rounded-full self-center" />
+          <div className="flex flex-col text-left self-center mx-2">
+          <p className="font-semibold text-md text-gray-600">The Light bulb</p>
+            <span className="text-xs">
               A purposeful area for the formation....
             </span>
           </div>
-        </div>
-        </Card>
-        <div className="flex justify-items-start my-3">
-          <img src={bulb} alt="bulb" className="w-14 h-14 rounded self-center" />
-          <div className="flex flex-col text-left mx-2">
-          <p className="font-semibold text-lg text-gray-600">The Light bulb</p>
-            <span className="hidden">
-    
-              A purposeful area for the formation....
-            </span>
-          </div>
-        </div>
-        <div className="flex justify-items-start my-3">
-          <img src={bulb} alt="bulb" className="w-14 h-14 rounded self-center" />
-          <div className="flex flex-col text-left mx-2">
-            <p className="font-semibold text-lg text-gray-600">The Light bulb</p>
-            <span className="hidden">
-              A purposeful area for the formation....
-            </span>
-          </div>
-        </div>
- 
+        </div> 
+      </Card>
+  
       </div>
       <div className="my-3">
-        <p className="font-semibold text-xl">Discover more bucket spaces</p>
+        <p className="font-semibold text-xl">Discover more buckets</p>
+        <Card>
         <div className="flex justify-items-start my-3">
-          <img src={bulb} alt="bulb" className="w-14 h-14 rounded self-center" />
-          <div className="flex flex-col text-left mx-2">
-          <p className="font-semibold text-lg text-gray-600">The Light bulb</p>
-            <span className="hidden">
-    
+          <img src={bulb} alt="bulb" className="w-14 h-14 rounded-full self-center" />
+          <div className="flex flex-col text-left self-center mx-2">
+          <p className="font-semibold text-md text-gray-600">The Light bulb</p>
+            <span className="text-xs">
               A purposeful area for the formation....
             </span>
           </div>
-        </div>
+        </div> 
+      </Card>
+      <Card>
         <div className="flex justify-items-start my-3">
-          <img src={bulb} alt="bulb" className="w-14 h-14 rounded self-center" />
-          <div className="flex flex-col text-left mx-2">
-          <p className="font-semibold text-lg text-gray-600">The Light bulb</p>
-            <span className="hidden">
-    
+          <img src={bulb} alt="bulb" className="w-14 h-14 rounded-full self-center" />
+          <div className="flex flex-col text-left self-center mx-2">
+          <p className="font-semibold text-md text-gray-600">The Light bulb</p>
+            <span className="text-xs">
               A purposeful area for the formation....
             </span>
           </div>
-        </div>
+        </div> 
+      </Card>
+      <Card>
         <div className="flex justify-items-start my-3">
-          <img src={bulb} alt="bulb" className="w-14 h-14 rounded self-center" />
-          <div className="flex flex-col text-left mx-2">
-          <p className="font-semibold text-lg text-gray-600">The Light bulb</p>
-            <span className="hidden">
-    
+          <img src={bulb} alt="bulb" className="w-14 h-14 rounded-full self-center" />
+          <div className="flex flex-col text-left self-center mx-2">
+          <p className="font-semibold text-md text-gray-600">The Light bulb</p>
+            <span className="text-xs">
               A purposeful area for the formation....
             </span>
           </div>
-        </div>
- 
+        </div> 
+      </Card>
       </div>
     </div>
   );
