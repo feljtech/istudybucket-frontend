4f0f6057adaedc24d6023455f6558440bde62f7a
Ndi Lionel <59184392+spykelion@users.noreply.github.com>
2021-10-13 18:29:03 +0100

    update subscribe on post section

--- src/components/Post/PostBase.js
+++ src/components/Post/PostBase.js
@@ -4,6 +4,7 @@ import {
   ArrowDownIcon,
   ArrowUpIcon,
   UserCircleIcon,
+  BellIcon,
 } from "@heroicons/react/solid";
 import me from "../../assets/img/me.jpeg";
 import TextArea from "../../__sub__/TextArea";
@@ -30,6 +31,11 @@ const PostBase = ({ children, ...props }) => {
 
   return (
     <div className="rounded border px-5 py-2 mt-5" {...props}>
+      {/* <p
+        onClick={() => setShowSubscribe(false)}
+        className="absolute h-screen w-screen pointer-events-none"
+      ></p> */}
+
       <div className="flex justify-between mb-4">
         <div className="flex ">
           <UserCircleIcon className="text-gray-400 w-12" />
@@ -47,8 +53,8 @@ const PostBase = ({ children, ...props }) => {
       </div>
       <div className="">{children}</div>
 
-      <div className="flex justify-between">
-        <div className="flex border-t pt-2">
+      <div className="flex justify-between border-t">
+        <div className="flex pt-2">
           <div className="flex justify-between mx-2">
             <div className="flex border border-gray-300 hover:bg-gray-400  rounded-full mr-2 p-1 cursor-pointer  ">
               <span className="text-gray-600 text-sm self-center">24</span>
@@ -78,8 +84,12 @@ const PostBase = ({ children, ...props }) => {
           />
           {showSubscribe && (
             <div className="relative bottom-7">
-              <p className="cursor-pointer text-white bg-green-500 hover:bg-green-700 p-1 absolute -top-6 right-0 text-xs ">
-                Subscribe
+              <p
+                onClick={() => setShowSubscribe(!showSubscribe)}
+                className="flex cursor-pointer border rounded-lg border-gray-300 hover:bg-gray-100 p-1 absolute -top-6 right-0"
+              >
+                <BellIcon className="text-gray-500 w-4" />
+                <span className=" text-sm ">subscribe</span>
               </p>
               <p className="absolute pointer-events-none -top-5 right-1 inline-block w-2 h-2 transform translate-x-1/2 -translate-y-1/2 bg-red-600 rounded-full"></p>
             </div>
