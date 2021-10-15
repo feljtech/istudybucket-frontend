f052aabf4c56cfe011382bc21cf689eba18fb692
Ndi Lionel <59184392+spykelion@users.noreply.github.com>
2021-10-15 22:37:57 +0100

    update textarea.Jsx

--- src/__sub__/TextArea.jsx
+++ src/__sub__/TextArea.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 // change to resizable when text reaches a specific length
 const TextArea = ({...props}) => {
     return ( 
-        <textarea {...props} className="outline-none block rounded mt-3" name="textfield" id="" cols="50" rows="3">
+        <textarea {...props} className="green-handle border border-gray-400 outline-none block rounded resize-none" name="textfield" id="" cols="50" rows="5">
 
         </textarea>
     )
